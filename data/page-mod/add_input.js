9a2f495d14e4fbe0a0612ef7d3a451c13e2baf7a
EdoPut <edoardo.putti@gmail.com>
2014-03-10 20:32:09 +0100

    Switched from pure JavaScript to Jquery-like zepto library

--- data/page-mod/add_input.js
+++ data/page-mod/add_input.js
@@ -1,45 +1,34 @@
-function addText(node, text){
-	node.appendChild(document.createTextNode(text));
-}
-function addRadio(node, obj){
-	var radio 	= document.createElement("input");
-	radio.id 	= obj.id;
-	radio.type 	= "radio";
-	radio.name 	= obj.name;
-	radio.value = obj.value;
-	radio.checked = obj.checked;
-	node.appendChild(radio);
-	addText(node, obj.text);
-}
-function customizeNode(node,obj){
-	var attribute_list = ["id", "class", "type", "multiple"];
-	for (var i = attribute_list.length - 1; i >= 0; i--) {
-		if(obj.hasOwnProperty(attribute_list[i])){
-			var attribute = attribute_list[i];
-			node.setAttribute(attribute, obj[attribute]);
-		}
-	}
-}
-function appendChildren(node, array){
-	for (var i = 0; i < array.length; i++) {
-		node.appendChild(array[i]);
-	}
-}
+var newForm     = $('<form></form>', {
+	id:'uploader',
+	class: 'full-width center'
+});
+$('<p></p>').html('Upload audio').appendTo(newForm);
+$('<input>', {
+	id:'radio_choice',
+	name: 'choice',
+	type: 'radio',
+	value: 'audio',
+	checked: 'checked'
+}).appendTo(newForm);
+$('<p></p>').html('Upload image').appendTo(newForm);
+$('<input>', {
+	id:'radio_choice',
+	name: 'choice',
+	type: 'radio',
+	value: 'photo'
+}).appendTo(newForm);
+$('<input>', {
+	id:'multi-upload',
+	class: 'center',
+	type: 'file',
+	multiple: 'multiple'
+}).appendTo(newForm);
+$('<button></button>', {
+	id:'upload-button',
+	type: 'button',
+	text: 'Click to upload!'
+}).on('click', function(event) {
+	console.log('Click!');
+}).appendTo(newForm);
 
-var newForm     = null;
-var input       = null;
-var button      = null;
-var content     = null;
-
-customizeNode(newForm	,{"id":"uploader"		, "class":"full-width center"});
-customizeNode(input		,{"id":"multi-upload"	, "class":"center", "type":"file", "multiple":"multiple"});
-customizeNode(button 	,{"id":"upload-button"	, "type":"button"});
-
-button.innerHTML = "Upload!"
-
-addRadio(newForm, {"id":"radio_choice", "name":"choice", "value":"audio","checked":"checked","text":"Upload audio"});
-addRadio(newForm, {"id":"radio_choice", "name":"choice", "value":"image","checked":""		,"text":"Upload image"});
-
-appendChildren(newForm,[input, button]);
-
-content.parentElement.insertBefore(newForm, document.getElementById("footer"));
+$('#content').append(newForm);
