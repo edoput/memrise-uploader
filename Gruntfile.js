687986b8f071c1e4de09d47a571074c34797db75
EdoPut <edoardo.putti@gmail.com>
2014-05-07 20:21:01 +0200

    Added mozilla-addon-sdk and mozilla-cfx-xpi task

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,11 +26,48 @@ module.exports = function (grunt) {
         'data/page-mod/*.js'
       ]
     },
-    "mozilla-addon-sdk": {
-      
+    //Define a mozilla addon-sdk to download and use
+    'mozilla-addon-sdk': {
+      '1_16': {
+        options: {
+          revision: '1.16',
+          dest_dir: 'build_tools/'
+        }
+      }
+    },
+    'mozilla-cfx-xpi': {
+      'stable': {
+        options: {
+          'mozilla-addon-sdk': '1_16',
+          extension_dir: '.',
+          dist_dir: 'tmp/dist-stable',
+          command: 'xpi',
+          arguments: '--strip-sdk' // builds smaller xpis
+        }
+      }
+    },
+    'mozilla-cfx': {
+      'run_stable': {
+        options: {
+          'mozilla-addon-sdk': '1_16',
+          extension_dir: '.',
+          command: 'run',
+          arguments: '-p ~/.local/addon_profile/default'
+        }
+      },
+      'run_nightly': {
+        options: {
+          'mozilla-addon-sdk': '1_16',
+          extension_dir: '.',
+          command: 'run',
+          arguments: '-b /mnt/sdb1/firefox/firefox -p ~/.local/addon_profile/nightly'
+        }
+      }
     }
   });
 
   grunt.registerTask('default', ['jshint', 'watch']);
+  grunt.registerTask('release', ['mozilla-cfx-xpi']);
+  grunt.registerTask('pre-release', ['mozilla-cfx:run_stable']);
 };
 
